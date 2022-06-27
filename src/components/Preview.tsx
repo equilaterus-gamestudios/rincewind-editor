3930bf55050d183a0da01d58b3ef6d62ba9ced4a
dacanizares <dacanizares@outlook.com>
2022-06-26 22:43:29 -0500

    Ignored type error

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,4 +1,6 @@
-import React, { useContext } from 'react'
+// @ts-nocheck
+// Ignoring style-types error
+import React, { CSSProperties, useContext } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
